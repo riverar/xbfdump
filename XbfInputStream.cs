e82277a188b4b073c8bdd5af269d82f4eec83aaf
Rafael Rivera <rafael@withinwindows.com>
2020-09-03 02:09:01 -0700

    Initial commit

---
+++ XbfInputStream.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace xbfdump
+{
+    class XbfInputStream : BaseXbfStream, IStreamType
+    {
+        public XbfInputStream(string filePath)
+        {
+            _backingStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        }
+
+        public StreamType StreamType {  get { return StreamType.Input;  } }
+    }
+}
